3c3919341beb7ae508b98811fcadd5dc718a3ebb
Wu-Kaixin <108617062+Wu-kaixin@users.noreply.github.com>
2026-09-16 15:55:59 +0800

    Export future decentralized interfaces

---
+++ src/abcg_controller/interfaces/__init__.py
@@ -0,0 +1,17 @@
+"""Extensibility interfaces."""
+
+from .decentralized import (
+    CrowdGroupHypothesis,
+    DecentralizedGuidePolicy,
+    DistributedResourceProposal,
+    LocalObservation,
+    NeighborMessage,
+)
+
+__all__ = [
+    "CrowdGroupHypothesis",
+    "DecentralizedGuidePolicy",
+    "DistributedResourceProposal",
+    "LocalObservation",
+    "NeighborMessage",
+]
